23a9f183213e82e37f9ef9658e3b89a906f92864
joeyz <joeyz5150@gmail.com>
2016-09-07 18:29:04 -0400

    Updated header viewport
    Updated the viewport meta tag for better scaling

--- header.php
+++ header.php
@@ -2,7 +2,8 @@
 <html <?php language_attributes(); ?> class="no-js">
 <head>
    <meta charset="<?php bloginfo( 'charset' ); ?>">
-   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+   <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
    <link rel="profile" href="http://gmpg.org/xfn/11">
    <?php wp_head(); ?>
 </head>
