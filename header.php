e92b623d7b2a986713899e54eab49616469d8033
joeyz <joeyz5150@gmail.com>
2016-09-07 18:32:58 -0400

    Added HTML5 Shim
    Added HTML5 Shim to the header for better IE support of html5 elements and media queries.

--- header.php
+++ header.php
@@ -5,6 +5,12 @@
    <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
    <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
    <link rel="profile" href="http://gmpg.org/xfn/11">
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
    <?php wp_head(); ?>
 </head>
 
